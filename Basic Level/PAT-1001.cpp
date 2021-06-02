591b693d3b822402a4ab32d47bba0c1ed7126e8f
叶梦飞 <70845636+mufiye@users.noreply.github.com>
2021-06-02 14:03:49 +0800

    Create PAT-1001.cpp

---
+++ Basic Level/PAT-1001.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    int n,ans=0;
+    cin>>n;
+    while(n!=1){
+       if(n%2 == 0) n=n/2;
+       else n=(3*n+1)/2;
+       ans++;
+    }
+    cout<<ans;
+}
