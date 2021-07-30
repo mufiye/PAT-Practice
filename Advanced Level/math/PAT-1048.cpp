6528245f7e54b3eb1f1157c28328f0b872449849
叶梦飞 <70845636+mufiye@users.noreply.github.com>
2021-07-30 11:15:30 +0800

    枚举问题，要考虑硬币面额最大是500

---
+++ Advanced Level/math/PAT-1048.cpp
@@ -0,0 +1,25 @@
+//因为硬币的面额最大就是500,但是硬币数目最多是10^5,所以考虑使用散列表的思想
+//柳神无敌
+#include <iostream>
+using namespace std;
+int a[1001];
+int main() {
+    int n, m, temp;
+    scanf("%d %d", &n, &m);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &temp);
+        a[temp]++;
+    }
+    for(int i = 0; i < 1001; i++) {
+        if(a[i]) {
+            a[i]--;
+            if(m > i && a[m-i]) {
+                printf("%d %d", i, m - i);
+                return 0;
+            }
+            a[i]++;
+        }
+    }
+    printf("No Solution");
+    return 0;
+}
