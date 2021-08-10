564fb39e2fedd0b7c2c394d41dfc9cbfccc96ddb
叶梦飞 <70845636+mufiye@users.noreply.github.com>
2021-08-10 10:26:19 +0800

    Update 二分查找.cpp

--- Advanced Level/一些范式代码/二分查找.cpp
+++ Advanced Level/一些范式代码/二分查找.cpp
@@ -2,7 +2,7 @@
 int binarySearch(int A[], int left, int right, int x){
     int mid;
     while(left <= right){
-        mid = (left + mid) / 2;
+        mid = left + (right - left)/2; //效果同mid=(left+right)/2,但是这样可以防止溢出
         if(A[mid] == x)  return mid;
         else if(A[mid] > x){
             right = mid - 1;
@@ -12,4 +12,26 @@ int binarySearch(int A[], int left, int right, int x){
         }
     }
     return -1;
+}
+
+//如何求序列中第一个大于等于x的元素的位置
+int lower_bound(int A[], int left, int right){
+    int mid;
+    while(left < right){
+        mid = (left + right)/2;
+        if(A[mid] >= x)  right = mid;
+        else  left = mid+1;
+    }
+    return left;
+}
+
+//如何求序列中第一个大于x的元素的位置
+int upper_bound(int A[], int left, int right){
+    int mid;
+    while(left < right){
+        mid = (left+right) / 2;
+        if(A[mid] > x)  right = mid;
+        else  left = mid+1
+    }
+    return left;
 }
