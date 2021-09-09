2366cc4c7f59cc824f819ffe1c0e259bc40cf72f
叶梦飞 <70845636+mufiye@users.noreply.github.com>
2021-09-09 21:29:38 +0800

    未完

---
+++ Advanced Level/2021-spring/1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int N, maxP;
+
+//dfs
+
+int main(){
+    //先根据maxP做一个质数表
+    /*
+      输出信息:
+      如果存在, 首先输出差距最大的序列, 之后输出首个数最大的数
+      如果不存在, 则给定范围内输出最大的质数
+    */
+    return 0;
+}
